1e3f65dc71b80e746e99a19796b593240eea1ef5
chriskrieg <chriskrieg95@gmail.com>
2019-10-29 16:52:04 -0400

    moved the if-else in my main to Player class, using the fight(player1, player2) method

--- src/co/grandcircus/Player.java
+++ src/co/grandcircus/Player.java
@@ -12,5 +12,24 @@ public abstract class Player {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public static void fight(Player player1, Player player2) {
+		String player1Result = player1.generateRoshambo().toString();
+		String player2Result = player2.generateRoshambo().toString();
+		if (player1Result == player2Result) {
+			System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, "Draw!");
+		} else if (player1Result.equalsIgnoreCase("rock") && player2Result.equalsIgnoreCase("scissors")) {
+			System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player1.getName() + " wins!");
+		} else if (player1Result.equalsIgnoreCase("paper") && player2Result.equalsIgnoreCase("rock")) {
+			System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player1.getName() + " wins!");
+		} else if (player1Result.equalsIgnoreCase("scissors") && player2Result.equalsIgnoreCase("paper")) {
+			System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player1.getName() + " wins!");
+		} else if (player1Result.equalsIgnoreCase("rock") && player2Result.equalsIgnoreCase("paper")) {
+			System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player2.getName() + " wins!");
+		} else if (player1Result.equalsIgnoreCase("paper") && player2Result.equalsIgnoreCase("scissors")) {
+			System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player2.getName() + " wins!");
+		} else if (player1Result.equalsIgnoreCase("scissors") && player2Result.equalsIgnoreCase("rock")) {
+			System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player2.getName() + " wins!");
+		}
+	}
 
 }
--- src/co/grandcircus/RoshamboApp.java
+++ src/co/grandcircus/RoshamboApp.java
@@ -25,25 +25,8 @@ public class RoshamboApp {
 			} else {
 				System.out.println("Please enter t or l: ");
 			}
+			Player.fight(player1, player2);
 			
-			String player1Result = player1.generateRoshambo().toString();
-			String player2Result = player2.generateRoshambo().toString();
-			if (player1Result == player2Result) {
-				System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, "Draw!");
-			} else if (player1Result.equalsIgnoreCase("rock") && player2Result.equalsIgnoreCase("scissors")) {
-				System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player1.getName() + " wins!");
-			} else if (player1Result.equalsIgnoreCase("paper") && player2Result.equalsIgnoreCase("rock")) {
-				System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player1.getName() + " wins!");
-			} else if (player1Result.equalsIgnoreCase("scissors") && player2Result.equalsIgnoreCase("paper")) {
-				System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player1.getName() + " wins!");
-			} else if (player1Result.equalsIgnoreCase("rock") && player2Result.equalsIgnoreCase("paper")) {
-				System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player2.getName() + " wins!");
-			} else if (player1Result.equalsIgnoreCase("paper") && player2Result.equalsIgnoreCase("scissors")) {
-				System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player2.getName() + " wins!");
-			} else if (player1Result.equalsIgnoreCase("scissors") && player2Result.equalsIgnoreCase("rock")) {
-				System.out.printf("\n%s: %s \n%s: %s \n%s\n", player1.getName(), player1Result, player2.getName(), player2Result, player2.getName() + " wins!");
-			}
-
 			// your logic should stop here if it doesn't need to be included in the loop
 			System.out.println("\nPlay again? (yes/no)");
 			cont = scnr.nextLine();
